81b074f1cf6ce21a1639d74dbd1a3fc8d8f6f7f2
Alan Vitor Rocha <alanv.rocha4@gmail.com>
2016-08-09 09:16:33 -0300

    Voltando para testar

--- src/android/Library/src/CameraActivity.java
+++ src/android/Library/src/CameraActivity.java
@@ -60,11 +60,16 @@ public class CameraActivity extends Activity {
 
     		List<String> focusModes = params.getSupportedFocusModes();
     		if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-    			// set the focus mode
+    			
+                // set the focus mode
     			params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-    			// set Camera parameters
+
+                //set quality
+                params.setJpegQuality(50);
+    			
+                // set Camera parameters
     			c.setParameters(params);
-    		}
+
 	    }
 	    catch (Exception e){
 	        // Camera is not available (in use or does not exist)
