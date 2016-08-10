5ff4861075ece73b2c6027547810f65f1864dc0e
Alan Vitor Rocha <alanv.rocha4@gmail.com>
2016-08-10 10:28:20 -0300

    afdas

--- src/android/Library/src/CameraActivity.java
+++ src/android/Library/src/CameraActivity.java
@@ -125,7 +125,7 @@ public class CameraActivity extends Activity {
                 public void onClick(View v) {
                 	Log.d(TAG, "CLICOU");
                 	Intent data = new Intent();
-                	Log.d(TAG, "==================TOTAL IMAGENS:"+ imgsPath.size().toString() +" ======");
+                	Log.d(TAG, "==================TOTAL IMAGENS:"+ imgsPath.size() +" ======");
                 	if(imgsPath.size() > 0){
                 		Bundle res = new Bundle();
                         res.putStringArrayList("MULTIPLEFILENAMES", imgsPath);
